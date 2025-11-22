73466d1b9ce1754bbcf8cac86cd4fca0289d6a6c
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-11-23 05:14:28 +0530

    Remove deprecated RDS and Cognito configurations from Terraform files

--- terraform/prod/security.tf
+++ terraform/prod/security.tf
@@ -154,49 +154,6 @@ resource "aws_security_group" "workers" {
   }
 }
 
-# RDS Security Group
-# resource "aws_security_group" "rds" {
-#   name        = "kubestock-sg-rds"
-#   description = "Security group for RDS PostgreSQL - access from K8s nodes and bastion"
-#   vpc_id      = aws_vpc.kubestock_vpc.id
-
-#   ingress {
-#     description     = "PostgreSQL from control plane"
-#     from_port       = 5432
-#     to_port         = 5432
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.control_plane.id]
-#   }
-
-#   ingress {
-#     description     = "PostgreSQL from workers"
-#     from_port       = 5432
-#     to_port         = 5432
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.workers.id]
-#   }
-
-#   ingress {
-#     description     = "PostgreSQL from bastion"
-#     from_port       = 5432
-#     to_port         = 5432
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.bastion.id]
-#   }
-
-#   egress {
-#     description = "Allow all outbound"
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   tags = {
-#     Name = "kubestock-sg-rds"
-#   }
-# }
-
 # NLB API Security Group
 resource "aws_security_group" "nlb_api" {
   name        = "kubestock-sg-nlb-api"
